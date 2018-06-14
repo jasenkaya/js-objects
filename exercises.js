e8d204d6b5d9ece9e1b98eb4e1359fb3cb22f402
Jasen Kaya <jasenkaya@Jasens-MacBook-Pro.local>
2018-06-13 21:50:45 -1000

    pau number 1

--- exercises.js
+++ exercises.js
@@ -14,7 +14,15 @@ Objects in Javascript can be compared to objects in real life. Objects has a col
     Console.log the object.
 */
     
-  
+  var books = {
+      title: "the obstacle is the way",
+      author: "Ryan Holiday",
+    category: "Self Help",
+    pages: 200
+  }
+
+  console.log(books)
+
 /*
 2.  Declare a variable named `dog` and create the following properties (key-value pairs) to the object:
    
